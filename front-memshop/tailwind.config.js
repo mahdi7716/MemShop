dce24e3d2a6c5b059af3961de46afb9c4f7e9852
mahdi7716 <32459776+mahdi7716@users.noreply.github.com>
2024-07-04 12:05:00 +0330

    1204
    1204

---
+++ front-memshop/tailwind.config.js
@@ -0,0 +1,19 @@
+const { Oswald, Urbanist } = require('next/font/google');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    fontFamily:{
+      oswald:"Oswald,sans-serif",
+      urbanist:"Urbanist,sans-serif"
+    },
+    extend: {
+    },
+  },
+  plugins: [],
+};
