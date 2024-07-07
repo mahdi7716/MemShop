ad14a2cf84856851ac8463c6952b0a1fe2fb07e9
mahdi7716 <32459776+mahdi7716@users.noreply.github.com>
2024-07-07 12:34:32 +0330

    1233
    1233

--- front-memshop/pages/_app.js
+++ front-memshop/pages/_app.js
@@ -1,5 +1,21 @@
 import "@/styles/globals.css";
-
+import Header from "@/Components/Header";
+import Footer from "@/Components/Footer";
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=
+              Oswald:wght@200..700&family=Roboto:ital,
+              wght@0,100;0,300;0,400;0,500;0,700;0,900
+              ;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+        rel="stylesheet"
+      ></link>
+      <Header/>
+      <Component {...pageProps} />
+      <Footer/>
+    </>
+  );
 }
