21d793d13a1bc386b7a9d41a039e7cb05d3ca5bd
mahdi7716 <32459776+mahdi7716@users.noreply.github.com>
2024-07-19 15:00:36 -0700

    0130
    0130

--- front-memshop/pages/index.js
+++ front-memshop/pages/index.js
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import HeroBanner from "@/Components/HeroBanner";
+import Wrapper from "@/Components/Wrapper";
 
 
 
@@ -7,8 +9,23 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className="h-[2000px]" >
-     Home Page   
+    <main className="" >
+    <HeroBanner/>  
+    <Wrapper>
+      <div className="text-center ax-w-[800px] mx-auto my-[50px] md:my-[80px]">
+        <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
+          Cushioning For Your Miles
+        </div>
+        <div className="text-md md:text-xl">
+          
+        in publishing and graphic design, Lorem ipsum is a
+        placeholder text commonly used to demonstrate the
+        visual form of a document or a typeface without 
+        relying on meaningful content.
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0"></div>
+    </Wrapper>
     </main>
   );
 }
